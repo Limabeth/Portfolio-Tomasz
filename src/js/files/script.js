f3a3cb35d5fb9251df3e94ffaa4f60286fe22640
Vladislav Iskakov <71043551+Limabeth@users.noreply.github.com>
2022-04-21 02:43:40 +0800

    Initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,16 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from './functions.js';
+// Подключение списка активных модулей
+import { flsModules } from './modules.js';
+
+import './utilities.js';
+
+import initMobileMenu from './menu.js';
+import initFilterizr from '../../modules/sections/portfolio/portfolio.js';
+import countTextareaCharacters from '../../modules/sections/contact/contact.js';
+
+(function init() {
+  initMobileMenu();
+  initFilterizr();
+  countTextareaCharacters();
+})();
